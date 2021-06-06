96dc1a9aa03fa34838fc17fdc5edc5246e357a70
kiranTh <thanna753@gmail.com>
2021-06-06 19:26:22 +0700

    ✨ update html

--- my_profile/core/views.py
+++ my_profile/core/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    html = "<h1>Hello, jjj<h1>"
-    return HttpResponse(html)
+    return render(request, 'profile.html')
