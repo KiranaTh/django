b735a6ab6225505f7cceabccda3126fde1e2433f
kiranTh <thanna753@gmail.com>
2021-06-08 10:46:07 +0700

    🎉 hw 2

--- my_profile/core/models.py
+++ my_profile/core/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    name = models.CharField(max_length=300)
+    fullname = models.CharField(max_length=300)
+    birthDate = models.CharField(max_length=300)
+    age = models.CharField(max_length=300)
+    education = models.CharField(max_length=300)
+    university = models.CharField(max_length=300)
+    workplace = models.CharField(max_length=300)
+    position = models.CharField(max_length=300)
+    pic_url = models.CharField(max_length=300)
+    facebook_url = models.CharField(max_length=300)
+    instragram_url = models.CharField(max_length=300)
+    github_url = models.CharField(max_length=300)
+
+class Email(models.Model):
+    email = models.CharField(max_length=300)
+    
+
--- my_profile/core/views.py
+++ my_profile/core/views.py
@@ -1,5 +1,70 @@
 from django.shortcuts import render
+from django.views import View
+
+from core.forms import SubscriberForm
+from core.models import Profile, Email
 
 # Create your views here.
-def index(request):
-    return render(request, 'profile.html')
+
+
+# def index(request):
+#     return render(request, 'profile.html')
+
+
+class IndexView(View):
+    def get(self, request):
+        profile = Profile.objects.get(id=4)
+        name = profile.name
+        form = SubscriberForm()
+        fullname = profile.fullname
+        birthDate = profile.birthDate
+        age = profile.age
+        education = profile.education
+        university = profile.university
+        workplace = profile.workplace
+        position = profile.position
+        pic_url = profile.pic_url
+        facebook_url = profile.facebook_url
+        instragram_url = profile.instragram_url
+        github_url = profile.github_url
+        return render(
+            request, 
+            'profile.html', 
+            {
+                "name": name,
+                "form": form,
+                "fullname": fullname,
+                "birthDate": birthDate,
+                "age": age,
+                "education": education,
+                "university": university,
+                "workplace": workplace,
+                "position": position,
+                "pic_url": pic_url,
+                "facebook_url": facebook_url,
+                "instragram_url": instragram_url,
+                "github_url": github_url,
+            }
+        )
+
+    def post(self, request):
+        print(request.POST)
+        print(request.POST.get("email"))
+        newEmail = request.POST.get("email")
+        Email.objects.create(email=newEmail)
+
+        profile = Profile.objects.get(id=4)
+        form = SubscriberForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            print(form.cleaned_data.get("email"))
+
+        name = profile.name
+        
+        return render(
+            request,
+            'thank.html',
+            {
+                "name": name,
+            }
+        )
